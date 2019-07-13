a45bf1564407965c78c010486a4eb0361703edfe
vishal <vishal.vaitla@gmail.com>
2019-07-13 23:58:26 +0530

    verification of r

--- LoopManager.py
+++ LoopManager.py
@@ -44,7 +44,7 @@ def selectschools():
         d = dict()
         d['c_name'] = c_name
         d['schools_present'] = sel_list
-
+        d['school_count'] = len(sel_lis t)
         ref = db.child("clusters").push(d)
         clust_id = ref['name']
         for school in sel_list:
@@ -54,9 +54,19 @@ def selectschools():
             })
         return redirect('/admin/dashboard')
 
-
-
-
+@app.route("/admin/verify_resources", methods=['POST','GET'])
+def verifyresources():
+    if request.method == 'POST':
+        return redirect("admin/cluster")
+    g = db.child("loopman").child("Q7F9y3WfP4VONOlNoLYTzJuHjSw2").child("requests").shallow().get()
+    f = []
+    for i in g:
+        gg = i
+        f.append({
+        id:gg,
+        res:db.child("loopman").child("Q7F9y3WfP4VONOlNoLYTzJuHjSw2").child("requests").child(gg).get().val()
+        })
+    return render_template("verify_resources.html", f = f)
 
 if __name__ == "__main__":
     app.run(debug = True)
